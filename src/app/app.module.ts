b0ccf3175a6493e6cc6447981ab20098106c6bdf
Aric Dayan Gutierrez Sanchez <adgsgutierrez@gmail.com>
2020-01-27 17:24:10 -0500

    Adicion de Firebase y Cordova con Android 8.0 o superior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { APPLICATION } from './../utils/constans';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,6 +6,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { AngularFireModule } from 'angularfire2';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -18,6 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(APPLICATION.firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/utils/constans.ts
@@ -0,0 +1,11 @@
+export const APPLICATION = {
+  firebaseConfig : {
+    apiKey: "AIzaSyA92zWn0Jv6VhfuG328dpyyCJa7CYsgE9o",
+    authDomain: "roadmapbeta.firebaseapp.com",
+    databaseURL: "https://roadmapbeta.firebaseio.com",
+    projectId: "roadmapbeta",
+    storageBucket: "roadmapbeta.appspot.com",
+    messagingSenderId: "1025652995601",
+    appId: "1:1025652995601:web:37c0ef601bf7b55971a3b0"
+  }
+}
